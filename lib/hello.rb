88fb1a69e7b93bece89d0aa49acee88e933f7e81
doulighan <doulighan@gmail.com>
2017-04-12 12:03:50 -0400

    done

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,15 @@
-def hello_t
-
+def hello_t(names)
+	i = 0
+	t_names = []
+	while i < names.length
+		if names[i].start_with?("T") || names[i].start_with?("t")
+			t_names << names[i]
+			yield names[i] 
+		end
+		i += 1
+	end
+	t_names
 end
 
-# call your method here!
+
 
